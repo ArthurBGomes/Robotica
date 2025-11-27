65bbfc2e87b4b6ef36de52ff91744b7e3d675b7c
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-11-27 09:11:54 -0300

    Create codecomp.ino

---
+++ LerSensorRefletancia/codecomp.ino
@@ -0,0 +1,71 @@
+#include <robo_hardware2.h> 
+#include <Servo.h>
+
+//-----PINOS PARA SENSORES REFLETANCIA-----//
+//SENSOR_LINHA_MAIS_ESQUERDO	    A3
+//SENSOR_LINHA_ESQUERDO			    A2
+//SENSOR_LINHA_DIREITO			    A1	
+//SENSOR_LINHA_MAIS_DIREITO			A0	
+//-----PINOS PARA SENSORES REFLETANCIA-----//
+
+float valorSensorMaisEsq;
+float valorSensorEsq;
+float valorSensorDir;
+float valorSensorMaisDir;
+
+void setup(){
+	Serial.begin(9600);
+	robo.configurar();
+}
+void loop(){
+	valorSensorMaisEsq = robo.lerSensorLinhaEsq(); 
+  valorSensorEsq = robo.lerSensorLinhaEsq();         //le um valor do sensor. O sensor retornar um valor de 0 100
+	valorSensorDir = robo.lerSensorLinhaDir();         //le um valor do sensor. O sensor retornar um valor de 0 100
+	valorSensorMaisDir = robo.lerSensorLinhaDir();
+	Serial.print("; Maisesq: ");
+	Serial.println(valorSensorMaisEsq);
+  Serial.print("; esq: ");
+	Serial.println(valorSensorEsq);       //Imprime o valor do sensor pela porta serial
+	Serial.print("; dir: ");
+	Serial.println(valorSensorDir);       //Imprime o valor do sensor pela porta serial
+	Serial.print("; Maisdir: ");
+	Serial.println(valorSensorMaisDir); 
+	//Impressão dos valores dos sensores
+	if (valorSensorMaisEsq > 50 && valorSensorEsq > 50 && valorSensorDir > 0.7 &&  valorSensorMaisDir > 50) {//todos pegando branco
+		robo.acionarMotores(70, 70);//frente
+		delay(500);
+	}
+	else if (valorSensorMaisEsq > 50 && valorSensorEsq <= 50 &&  valorSensorDir <= 0.7  && valorSensorMaisDir > 50) {//só os mais pegando branco
+		robo.acionarMotores(70, 70);//frente
+		delay(500);
+	}
+  	else if ( valorSensorMaisEsq <= 50 && valorSensorEsq <= 50 && valorSensorDir <= 0.7 && valorSensorMaisDir > 50) {//só o mais direito pegando branco
+		robo.acionarMotores(-70, 70);//esquerda
+		delay(500);
+	} else if (valorSensorMaisEsq <= 50 && valorSensorEsq <= 50  &&  valorSensorDir  > 0.7  && valorSensorMaisDir > 50) {//só os direitos pegando branco
+		robo.acionarMotores(-70, 70);//esquerda
+		delay(500);
+	} else if (valorSensorMaisEsq > 50 && valorSensorEsq <= 50 && valorSensorDir <= 0.7 &&  valorSensorMaisDir <= 50) { //só o mais esquerdo pegando branco
+		robo.acionarMotores(70, -70);//direita
+		delay(500);
+	}
+  	else if (valorSensorMaisEsq > 50 && valorSensorEsq > 50 && valorSensorDir <= 0.7 &&  valorSensorMaisDir <= 50) {// só os esquerdos pegando branco
+		robo.acionarMotores(70, -70);//direita
+		delay(500);
+	}
+  	else if (valorSensorMaisEsq > 50 && valorSensorEsq > 50 && valorSensorDir > 0.7 &&  valorSensorMaisDir <= 50) {// só o mais direio pegando preto
+		robo.acionarMotores(70, -70);//direita
+		delay(500);
+	}
+  	else if (valorSensorMaisEsq <= 50 && valorSensorEsq <= 50 && valorSensorDir <= 0.7 &&  valorSensorMaisDir > 50) {// só o mais direito pegando branco
+		robo.acionarMotores(-70, 70);//esquerda
+		delay(500);
+	}
+	else {
+		robo.acionarMotores(0,0);//parar
+		delay(500);
+	}
+
+	delay(1000);
+
+}
